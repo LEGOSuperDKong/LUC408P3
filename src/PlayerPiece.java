3b8be0d7aff03c6ba60a97ef93dbd27afa1b844c
LEGOSuperDKong <LEGOSuperDKong@users.noreply.github.com>
2018-04-03 14:47:26 -0400

    Update PlayerPiece.java

--- src/PlayerPiece.java
+++ src/PlayerPiece.java
@@ -1,7 +1,18 @@
 package rpgio;
 
+import class Weapon;
+import class Tile;
+
 public class PlayerPiece {
     
-    
+    private int PlayerPieceID,
+    moveRange,
+    defense,
+    attack,
+    speed,
+    luck,
+    health;
+    Weapon pieceWeapon;
+    Tile pieceLocation;   
     
 }
