f976e31a022c0eaa7822a5189a0e23305bb5a29a
Tyler Cypher <tcyph33@gmail.com>
2018-04-03 14:56:53 -0400

    created vars and get methods for Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,1 +1,60 @@
 
+
+public class Tile {
+
+    //Create variables
+    private boolean chest,
+            int defense,
+            int moveCost,
+            boolean highlight,
+            int locationID,
+            int adjacentUp,
+            int adjacentDown,
+            int adjacentLeft,
+            int adjacentRight;
+
+    //constructor
+    public Tile(){
+
+    }
+
+
+    //get methods
+
+    public boolean getChest(){
+        return chest;
+    }
+
+    public int getDefense(){
+        return defense;
+    }
+
+    public int getMoveCost(){
+        return moveCost;
+    }
+
+    public boolean getHighlight(){
+        return highlight;
+    }
+
+    public int getLocationID(){
+        return locationID;
+    }
+
+    public int getAdjacentUp(){
+        return adjacentUp;
+    }
+
+    public int getAdjacentDown(){
+        return adjacentDown;
+    }
+
+    public int getAdjacentLeft()){
+        return adjacentLeft;
+    }
+
+    public int getAdjacentRight(){
+        return adjacentRight;
+    }
+
+}
