9e0cc20840c19550524d5198515419fdaf844e1e
Tyler Cypher <tcyph33@gmail.com>
2018-04-12 14:36:49 -0400

    moved gui to src

---
+++ src/gui.java
@@ -0,0 +1,18 @@
+package rpgio;
+
+import java.util.Random;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class gui extends JFrame{
+
+
+    gui()
+    {
+        setSize(800,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
