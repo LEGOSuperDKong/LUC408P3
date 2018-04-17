e6ceacd2d0d8787e7e6882779400b1b707e25406
LEGOSuperDKong <LEGOSuperDKong@users.noreply.github.com>
2018-04-17 15:43:15 -0400

    Update gui.java

--- src/rpgio/gui.java
+++ src/rpgio/gui.java
@@ -11,24 +11,61 @@ public class gui extends JFrame{
     gui()
     {
     	super("RPGio"); //Sets title
-        setSize(1000,800);
+        setSize(1920,1200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
 
-        JPanel gameBoard = new JPanel();
+        JLabel gameBoard = new JLabel();
+        JPanel gameBoardHolder = new JPanel();
+        //gameBoardHolder.setLayout(null);
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(null);
-
-        JButton btnMove = new JButton("Move");
-        JButton btnItem = new JButton("Use Item");
+        buttonPanel.setLayout(new GridBagLayout());
+        
+        ImageIcon movebg = new ImageIcon("moveButton.png");
+        ImageIcon itembg = new ImageIcon("itemButton.png");
+        ImageIcon turnbg = new ImageIcon("turnButton.png");
+        
+        JButton btnTurn = new JButton(turnbg);
+        JButton btnMove = new JButton(movebg);
+        JButton btnItem = new JButton(itembg);
 
         btnMove.setBounds(0, 0, 100, 100);
         btnItem.setBounds(100, 0, 100, 100);
-
-        buttonPanel.add(btnMove);
-        buttonPanel.add(btnItem);
-        add(buttonPanel);
-
+        
+        //move button
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        buttonPanel.add(btnMove,c);
+        
+        //item button
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 0;
+        buttonPanel.add(btnItem,c);
+        
+        //turn button
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+        buttonPanel.add(btnTurn, c);
+        
+        //panel of buttons added to frame
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        add(buttonPanel, c);
+        
+        ImageIcon gameMap = new ImageIcon("gameboardcopy.png");
+        gameBoard.setIcon(gameMap);
+        gameBoardHolder.add(gameBoard);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        add(gameBoardHolder,c);
     }
 
 }
